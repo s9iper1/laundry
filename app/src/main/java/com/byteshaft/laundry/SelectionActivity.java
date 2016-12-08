76dec6798032c1d25bb661fd172b3336d7e7046c
rb-shahid <shgr8rb@gmail.com>
2016-12-08 16:12:05 +0500

    add tabs structure

--- app/src/main/java/com/byteshaft/laundry/SelectionActivity.java
+++ app/src/main/java/com/byteshaft/laundry/SelectionActivity.java
@@ -1,13 +1,163 @@
 package com.byteshaft.laundry;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
-public class SelectionActivity extends AppCompatActivity {
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
+
+public class SelectionActivity extends AppCompatActivity implements MaterialTabListener {
+
+
+    private Toolbar mToolbar;
+    private MaterialTabHost mTabHost;
+    private ViewPager mViewPager;
+    private ViewPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+
+        mToolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        mTabHost = (MaterialTabHost) findViewById(R.id.materialTabHost);
+        mViewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        mAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mAdapter);
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                mTabHost.setSelectedNavigationItem(position);
+
+            }
+        });
+
+        for (int i = 0; i < mAdapter.getCount(); i++) {
+            MaterialTab materialTab = mTabHost.newTab();
+            materialTab.setText("Tab");
+            materialTab.setTabListener(this);
+            mTabHost.addTab(materialTab);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dynamic_tabs, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (R.id.add_tabs == id) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+            alert.setTitle("Add Tab");
+            alert.setMessage("Your Tab Name");
+
+// Set an EditText view to get user input
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    mTabHost.addTab(mTabHost.newTab().setText(input.getText().toString()).setTabListener(SelectionActivity.this));
+                    mTabHost.notifyDataSetChanged();
+                    mAdapter.setCount(mAdapter.getCount() + 1);
+
+                }
+            });
+
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(MaterialTab tab) {
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabReselected(MaterialTab tab) {
+
+    }
+
+    @Override
+    public void onTabUnselected(MaterialTab tab) {
+
+    }
+
+    public static class DummyFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                 @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.item_drawer, container, false);
+        }
+    }
+
+    private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+        int count = 1;
+        FragmentManager fragmentManager;
+
+        ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+            fragmentManager = fm;
+        }
+
+        public Fragment getItem(int num) {
+            DummyFragment dummyFragment = new DummyFragment();
+            return dummyFragment;
+
+        }
+
+        @Override
+        public int getCount() {
+            return count;
+        }
+
+        void setCount(int newCount) {
+            count = newCount;
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return getResources().getStringArray(R.array.tabs)[position];
+        }
     }
 }
