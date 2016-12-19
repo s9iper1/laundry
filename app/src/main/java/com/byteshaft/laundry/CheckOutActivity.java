69e39b58c6e1f7576719f7420fb1e89dbb5f8eb0
s9iper1 <s9iper1@gmail.com>
2016-12-19 12:48:38 +0500

    checkout activity startup

---
+++ app/src/main/java/com/byteshaft/laundry/CheckOutActivity.java
@@ -0,0 +1,15 @@
+package com.byteshaft.laundry;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CheckOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout);
+
+    }
+}
