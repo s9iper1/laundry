b38aac0bb2fef7ee8bd13ada3aad8946ff8f6fb6
hsunain hussi <husnain446@gmail.com>
2016-12-22 14:15:47 +0500

    get address for location, snakBar shows address

--- app/src/main/java/com/byteshaft/laundry/PickLaundryActivity.java
+++ app/src/main/java/com/byteshaft/laundry/PickLaundryActivity.java
@@ -1,12 +1,17 @@
 package com.byteshaft.laundry;
 
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -23,6 +28,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class PickLaundryActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener, LocationListener {
@@ -34,11 +43,21 @@ public class PickLaundryActivity extends FragmentActivity implements OnMapReadyC
     LatLng latLng;
     Marker currLocationMarker;
 
+    private CoordinatorLayout Clayout;
+
+    private Snackbar snack;
+    List<Address> addresses;
+    String address;
+    Double longitude;
+    Double latitude;
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pick_laundry);
+        Clayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -51,25 +70,55 @@ public class PickLaundryActivity extends FragmentActivity implements OnMapReadyC
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
+                mMap.clear();
                 mMap.addMarker(new MarkerOptions()
                         .position(latLng).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+
                 );
+
+                longitude = latLng.longitude;
+                latitude = latLng.latitude;
                 System.out.println(latLng + "Position");
                 String Postion = "" + latLng;
+                System.out.println(longitude + "longitude");
+                System.out.println(latitude + "latitude");
+                
+                snack = Snackbar.make(findViewById(R.id.coordinatorLayout),
+                        getCompleteAddressString(latitude, longitude), Snackbar.LENGTH_INDEFINITE);
+                CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams)
+                        snack.getView().getLayoutParams();
+                params.setMargins(20, 20, 20, 20);
+                snack.getView().setLayoutParams(params);
+                snack.show();
             }
         });
-
-        if (ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        }
-
-        mMap.setMyLocationEnabled(true);
         buildGoogleApiClient();
-
         mGoogleApiClient.connect();
 
+    }
+
+    private String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        String strAdd = "";
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1);
+            if (addresses != null) {
+                Address returnedAddress = addresses.get(0);
+                StringBuilder strReturnedAddress = new StringBuilder("");
+
+                for (int i = 0; i < returnedAddress.getMaxAddressLineIndex(); i++) {
+                    strReturnedAddress.append(returnedAddress.getAddressLine(i)).append("\n");
+                }
+                strAdd = strReturnedAddress.toString();
+                Log.w("My Current loction address", "" + strReturnedAddress.toString());
+            } else {
+                Log.w("My Current loction address", "No Address returned!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.w("My Current loction address", "Canont get Address!");
+        }
+        return strAdd;
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -79,6 +128,7 @@ public class PickLaundryActivity extends FragmentActivity implements OnMapReadyC
                 .addApi(LocationServices.API)
                 .build();
     }
+
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
@@ -99,13 +149,10 @@ public class PickLaundryActivity extends FragmentActivity implements OnMapReadyC
             currLocationMarker = mMap.addMarker(markerOptions);
         }
         mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(5000); //5 seconds
-        mLocationRequest.setFastestInterval(3000); //3 seconds
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         //mLocationRequest.setSmallestDisplacement(0.1F); //1/10 meter
 
         LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-
 
     }
 
@@ -131,11 +178,22 @@ public class PickLaundryActivity extends FragmentActivity implements OnMapReadyC
         markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
         currLocationMarker = mMap.addMarker(markerOptions);
 
+        if (counter < 1) {
+            longitude = latLng.longitude;
+            latitude = latLng.latitude;
+            snack = Snackbar.make(findViewById(R.id.coordinatorLayout),
+                    getCompleteAddressString(latitude, longitude), Snackbar.LENGTH_INDEFINITE);
+            CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams)
+                    snack.getView().getLayoutParams();
+            params.setMargins(20, 20, 20, 20);
+            snack.getView().setLayoutParams(params);
+            snack.show();
+        }
+        counter++;
         //zoom to current position:
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(latLng).zoom(16).build();
         mMap.animateCamera(CameraUpdateFactory
                 .newCameraPosition(cameraPosition));
-
     }
 }
