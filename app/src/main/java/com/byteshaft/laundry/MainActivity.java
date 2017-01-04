37e8327ae13d80c3fdd7a76cf1e8a40b883daac0
rb-shahid <shgr8rb@gmail.com>
2017-01-04 15:21:51 +0500

    add location activity

--- app/src/main/java/com/byteshaft/laundry/MainActivity.java
+++ app/src/main/java/com/byteshaft/laundry/MainActivity.java
@@ -55,8 +55,6 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 System.out.println("Hello there");
                 startActivity(new Intent(MainActivity.this, LaundryCategoriesActivity.class));
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
             }
         });
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -137,7 +135,7 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         switch (item.getItemId()) {
             case R.id.login:
-                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                startActivity(new Intent(getApplicationContext(), AddressesActivity.class));
                 break;
             case R.id.nav_update_profile:
                 loadFragment(new UpdateProfile());
